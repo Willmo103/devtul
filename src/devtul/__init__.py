e67f72ad97be145f727bff869babe675ef076dac
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2025-11-07 18:55:19 -0600

    feat: Bump version to 0.1.10 in project files

--- src/devtul/__init__.py
+++ src/devtul/__init__.py
@@ -4,6 +4,6 @@ DevTul - A collection of developer tools for working with git repositories.
 
 from .main import app, main
 
-__version__ = "0.1.5"
+__version__ = "0.1.10"
 
 __all__ = ["app", "main", "__version__"]
